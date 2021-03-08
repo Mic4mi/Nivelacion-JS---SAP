95e51d6d7a957744742a35fbd4ad976a7fb7d25a
Micaela Acuña <micahl1981@gmail.com>
2021-03-08 17:00:32 -0300

    Requerimientos esenciales funcionando
    1) Permite ingresar y validar datos para ser cargados en una lista: general, aprobada, desaprobada.
    2) Permite efectuar una búsqueda si solo si existen alumnos guardados en la lista, por diferentes parámetros (nombre, dni, telefono, email)
    3) Permite emitir una lista general, de aprobados o desaprobados

--- Ejercicio-25.js
+++ Ejercicio-25.js
@@ -1,5 +1,6 @@
 const APROBADOS = [],
-    DESAPROBADOS = [];
+    DESAPROBADOS = [],
+    GENERAL = [];
 let form = document.getElementById("registro"),
     nombre = document.getElementById("nombre"),
     DNI = document.getElementById("dni"),
@@ -17,7 +18,11 @@ let form = document.getElementById("registro"),
     historia = document.getElementById("historia"),
     biologia = document.getElementById("biologia"),
     informatica = document.getElementById("informatica"),
-    idiomas = document.getElementById("idiomas");
+    idiomas = document.getElementById("idiomas"),
+    selectValueBusqueda = document.getElementById("buscarPor"),
+    respuestaBusqueda = document.getElementById("resultadoBusqueda"),
+    selectValueLista = document.getElementById("filtrarPor"),
+    respuestaLista = document.getElementById("resultadoLista");
 
 
 // Validaciones
@@ -28,7 +33,78 @@ anio.addEventListener('blur', (event) => esAnioValido(event.target.value));
 telefono.addEventListener('blur', (event) => esTelefonoValido(event.target.value));
 form.addEventListener('submit', (event) => onSubmit(event, generarFechaString(dia.value, mes.value, anio.value)));
 
+
 // Declaración de funciones
+
+function ejecutarListado() {
+
+    if (GENERAL.length === 0) {
+        alert("No hay alumnos en el sistema")
+        return;
+    }
+
+    respuestaLista.value = '';
+
+    switch (selectValueLista.value) {
+        case "aprobados":
+            if (APROBADOS.length === 0) {
+                respuestaLista.value = 'No existen alumnos aprobados aun.';
+            }
+            listarAlumnos(APROBADOS, respuestaLista);
+            break;
+        case "desaprobados":
+            if (DESAPROBADOS.length === 0) {
+                respuestaLista.value = 'No existen alumnos desaprobados aun.';
+            }
+            listarAlumnos(DESAPROBADOS, respuestaLista);
+            break;
+        case "todos":
+            listarAlumnos(GENERAL, respuestaLista);
+            break;
+    }
+}
+
+function ejecutarBusqueda() {
+
+    if (GENERAL.length === 0) {
+        alert("No hay alumnos en el sistema")
+        return;
+    }
+
+    respuestaBusqueda.value = '';
+
+    switch (selectValueBusqueda.value) {
+        case "DNI":
+            respuestaBusqueda.value = '';
+            let dniUsuario = parseInt(prompt("Ingrese DNI"));
+            let dniExiste = buscarPorDNI(dniUsuario, GENERAL);
+            if (!dniExiste) {
+                respuestaBusqueda.value = "No existe alumno con ese DNI.";
+            }
+            break;
+        case "nombre":
+            let nombreUsuario = prompt("Ingrese nombre");
+            let nombreExiste = buscarPorNombre(nombreUsuario, GENERAL);
+            if (!nombreExiste) {
+                respuestaBusqueda.value = "No existe alumno con ese nombre.";
+            }
+            break;
+        case "celular":
+            let telefonoUsuario = parseInt(prompt("Ingrese telefono."));
+            let telefonoExiste = buscarPorCelular(telefonoUsuario, GENERAL);
+            if (!telefonoExiste) {
+                respuestaBusqueda.value = "No existe alumno con ese teléfono.";
+            }
+            break;
+        case "email":
+            let emailUsuario = prompt("Ingrese el correo electrónico.");
+            let emailExiste = buscarPorEmail(emailUsuario, GENERAL);
+            if (!emailExiste) {
+                respuestaBusqueda.value = "No existe alumno con ese correo electrónico.";
+            }
+            break;
+    }
+}
 
 function generarFechaString(dia, mes, anio) {
     return mes + "/" + dia + "/" + anio;
@@ -46,6 +122,9 @@ function onSubmit(event, fecha) {
 
     if (emailValido && esMayor && telefonoValido && sonLegajosIguales) {
         alumno = new Alumno(nombre.value, DNI.value, dia.value, mes.value, anio.value, telefono.value, email.value, curso.value, legajo.value, calificacion);
+
+        GENERAL.push(alumno);
+
         if (alumno.calificacionFinal >= 100) {
             APROBADOS.push(alumno);
         } else {
@@ -171,6 +250,65 @@ function limpiarFormulario(formulario) {
     formulario.reset();
 }
 
+function mostrarMenu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function listarAlumno(alumno) {
+    return `_Alumno_\nLegajo: ${alumno.legajo}\nCurso ${alumno.curso}\nDNI: ${alumno.dni}\nNombre: ${alumno.nombre}\nFecha de nacimiento: ${alumno.cumpleanios}\nTelefono: ${alumno.telefono}\nEmail: ${alumno.email}\nCalificacion: ${alumno.calificacionFinal}%\nObservaciones: ${alumno.mensajeDePromocion()}\n`;
+}
+
+function listarAlumnos(alumnos, textArea) {
+    for (let i = 0; i < alumnos.length; i++) {
+        textArea.value += listarAlumno(alumnos[i]);
+    }
+}
+
+function buscarPorDNI(dni, listaA) {
+    for (let i = 0; i < listaA.length; i++) {
+        if (dni == listaA[i].dni) {
+            respuestaBusqueda.value = listarAlumno(listaA[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function buscarPorNombre(nombre, listaA) {
+    for (let i = 0; i < listaA.length; i++) {
+        if (nombre.toLowerCase() === listaA[i].nombre.toLowerCase()) {
+            respuestaBusqueda.value = listarAlumno(listaA[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function buscarPorEmail(email, listaA) {
+    for (let i = 0; i < listaA.length; i++) {
+        if (email.toLowerCase() === listaA[i].email.toLowerCase()) {
+            respuestaBusqueda.value = listarAlumno(listaA[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function buscarPorCelular(celular, listaA) {
+    for (let i = 0; i < listaA.length; i++) {
+        if (celular == listaA[i].telefono) {
+            respuestaBusqueda.value = listarAlumno(listaA[i]);
+            return true;
+        }
+    }
+    return false;
+}
+
 function Alumno(nombre, dni, dia, mes, anio, telefono, email, curso, legajo, calificacionFinal) {
     this.nombre = nombre;
     this.dni = dni;
@@ -183,4 +321,11 @@ function Alumno(nombre, dni, dia, mes, anio, telefono, email, curso, legajo, cal
     this.legajo = legajo;
     this.calificacionFinal = calificacionFinal;
     this.cumpleanios = this.mes + "/" + this.dia + "/" + this.anio;
+    this.mensajeDePromocion = function () {
+        if (this.calificacionFinal >= 100) {
+            return "Alumno promueve exitosamente, pasó de año.";
+        } else {
+            return "Alumno no alcanza lo suficiente para poder promover, no pasa de año";
+        }
+    }
 }
